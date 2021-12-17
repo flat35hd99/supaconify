9b0c13ac3b77639efc67e8530ffa4e42f7db857e
flat35hd99 <38929316+flat35hd99@users.noreply.github.com>
2021-12-18 01:28:40 +0900

    Add metadata of this project

--- setup.py
+++ setup.py
@@ -5,9 +5,5 @@ setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     packages=find_packages(),
-    entry_points={
-        "console_scripts": [
-            "supaconify=supaconify.cli:cli"
-        ]
-    }
+    entry_points={"console_scripts": ["supaconify=supaconify.cli:cli"]},
 )

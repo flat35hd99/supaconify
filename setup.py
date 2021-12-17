9b4296d5011ff968155a1b485e55a3b25421e7f6
flat35hd99 <38929316+flat35hd99@users.noreply.github.com>
2021-12-18 01:25:09 +0900

    init

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+import versioneer
+
+setup(
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "supaconify=supaconify.cli:cli"
+        ]
+    }
+)
